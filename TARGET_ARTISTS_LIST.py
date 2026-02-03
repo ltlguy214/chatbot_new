40468946520e0b5965370f21d1b48343dabba058
Le Tran Thu Huyen <24410306@ms.uit.edu.vn>
2026-02-03 14:48:15 +0700

    Initial commit

---
+++ TARGET_ARTISTS_LIST.py
@@ -0,0 +1,291 @@
+# Tổng cộng: 288 nghệ sĩ trích xuất từ dữ liệu của bạn
+TARGET_ARTISTS = [
+    '7571XEqHqL61NDiLtWXhS5', # (S)TRONG
+    '0zo8kCJVktn7oPnzpkbC2p', # 14 Casper
+    '5a9t9Ace3c3WLhwlYXsTGH', # 2T
+    '1y28JlO6rDMUB5rkcA4RAh', # 52Hz
+    '2aQnC3DbZB9GbauvhAw7ve', # AMEE
+    '4wZ6awunqaaVLl5j0WpDFu', # ANH TRAI "SAY HI"
+    '0IBUlucigx80io4qV5fQ34', # An Vũ
+    '0tBmmwc8j8zbJxISwdsvjA', # Andiez
+    '4grjJqg7iwQ8RKHs8d9Snh', # Andree Right Hand
+    '0jF7Zlz8P5p74zcH7YwcMU', # Anh Quân Idol
+    '62D1qvKkvUCvXEtY1xwRfo', # Anh Tú Atus
+    '1WvNgEoB66jmHodcj15Zi9', # B Ray
+    '7FKQ3rlfjiDaZI0uQqOnDH', # Beta Remix
+    '3Tehj7YghQc7zH0I1faGc6', # BigDaddy
+    '2nSO7JYDbJrYbJmP39qUzj', # Binz
+    '5fa13NJjmn2uQ3dxZDi2Ge', # Bích Phương
+    '2Gvp79Cmni6PX13CAlSGex', # Bùi Anh Tuấn
+    '6oqtpFuvCTISMPeGv6P2sj', # Bùi Công Nam
+    '2XtMx7EHHODQSeBzDCBec9', # Bùi Lan Hương
+    '4fCHhderLwLacsIOIKgu3J', # Bảo Anh
+    '4uqZAHbX45ygzxhxkRwTwR', # Bệt Band
+    '1pKPrziZw7C4GvOlBIXqP5', # CAPTAIN BOY
+    '7MtgS1BXZAtslgSSTpdikk', # CODYNAMVO
+    '5M1fPnl4JpMMlb3Twc8etP', # CT
+    '2VUAd7F7SCWs7gYMGAQVxK', # Cao Thái Sơn
+    '1nX1HVIUo1Zfs3e2v3lFb9', # Changg
+    '6jFvKq4gMkQ50joURHPGXO', # Charmy Pham
+    '6jRD85dsXJYn5vv73rqSGi', # Chi Pu
+    '3TInd7W6K9AZ6lKYA3dviJ', # Chi Xê
+    '2xvW7dgL1640K8exTcRMS4', # Chillies
+    '2ZtffdcxTFHQhYa3HlNbTl', # Chung Thanh Duy
+    '0wQBc88XT0U6yRa23QFwV8', # Châu Dương
+    '1RZSSA978w2tUKtXqC6XGZ', # Châu Khải Phong
+    '61ydMyohF67xBZmWSPA3YQ', # Chọn gì? Chọn đi!
+    '4jpoAFITrFeFHeZ7Fgv3KZ', # Cloud 5
+    '1Qia4CTwh6x0CZbD0079zi', # CoolKid
+    '5L9ssI4l4DKU1za4ZwvKvG', # DTAP
+    '6zUWZmyi5MLOEynQ5wCI5f', # Da LAB
+    '5xY6E5PMZNtz9jDvxTRiGI', # Dangrangto
+    '2wFLGhWSXQEk1xa3FRusBN', # Danh Tuấn Trung
+    '4Xev3lOT3J5LZ3zQaK8sAz', # DanhKa
+    '6mMxOJfr3bN1QPUiForaWf', # DatKaa
+    '2Sq7FiVRRiu0Djsva7iV85', # Dee Trần
+    '4zg9WrDAFl1dGh2eSWLnOG', # Dfoxie37
+    '1NM99SQTX93oCkzvNXWJCf', # Double2T
+    '52UlQPpkuAzH3BTfRDevYc', # Doãn Hiếu
+    '196qGzmjvUJDtjKjO2604d', # Du Uyên
+    '1URs0dKndWOld1wzAFhszR', # Dunghoangpham
+    '3JkGKNawown8MgcJsDw1WT', # Duongg
+    '4KPyQxL1zqEiBcTwW6c9HE', # Dương Domic
+    '5HsNpclQf1lxU1XulbOB1Z', # Dương MasterD
+    '3l6YiTosGtrsp8iGs0IASz', # Dương Ái Vy
+    '1I5CkmtLVWLUygTT3cLr5X', # EM XINH "SAY HI"
+    '1L1VfizWn4DkFt602yD80U', # ERIK
+    '4bPYvyUn1mWwUwIW57oST2', # Emcee L (Da LAB)
+    '7kSmS2G15QIw8rllwGex9r', # Fishy
+    '1fTWz0OemWveF9AMkRPJKi', # Forest Studio
+    '6d0dLenjy5CnR5ZMn2agiV', # GREY D
+    '2NYFphd3cUQ5pLEVLaBP9L', # GiGi Hương Giang
+    '4ElDPQpWtnQr7qz6QxzKw9', # Gia Huy Singer
+    '5sK9cYMRqNOdY9Og2DKAgs', # H2O Remix
+    '5HZtdKfC4xU0wvhEyYDWiY', # HIEUTHUHAI
+    '7sI2avJ3MyFstC9oKNM7py', # Han Sara
+    '1vHY0HztdnDYKIUB7bkVkb', # Hana Cẩm Tiên
+    '7tRkypqswDC2uWg9g9H0Vs', # Hannie
+    '36HqaJcdxxKup5AbhP87k5', # Hiya
+    '6ZmHJcmCcc9X2sAyz6cHng', # Hiền Hồ
+    '7kBsX15IDun9YxuVmKCMYJ', # Hngle
+    '516yIO7zjKJCIkRktGVUw5', # Hoa Vinh
+    '3BWBxpXDxofgji3RKZPIz8', # Hoà Minzy
+    '2dlC6p7Q75wnfhudwTEpxW', # Hoài Lâm
+    '3LbBKYlyMaATS8IIe7HJ0d', # Hoàng Duyên
+    '6OzE2OdvV2tGAxSBsBuZ74', # Hoàng Dũng
+    '0r63ReVRjxrS4ATbLrdcrL', # Hoàng Thùy Linh
+    '3fM2WF0qY1Ip9ffVvOzhjo', # Hoàng Thống
+    '6xRZoWlE9twEkMC5NW4Z9f', # Hoàng Tôn
+    '3FwYnbtGNt8hJfjNuOfpeG', # Hà Anh Tuấn
+    '3XOuN4VDN4EL0GjKov5mWL', # Hà Nhi
+    '1L2VD3TPgcbUod5IID06xJ', # Hương Giang
+    '3NckpPMWZSEus6PyV6LoVc', # Hương Ly
+    '0F1rSmrNT6sp3LYohgYKsT', # Hải Đăng Doo
+    '24Wn81dwdDeTCuB1BWGoVJ', # Hồ Ngọc Hà
+    '3bm8AMLokf68Zpr5rRpPps', # Hồ Nhi
+    '0XOHQOteNO7pqYk5hWs9TX', # Hồ Việt Trung
+    '3Ufoo4BPShhahtCSjgpBLP', # Hứa Kim Tuyền
+    '2uv4Nr3UgSbEa9acPTJcVX', # Inso
+    '40JnMfFlpwqBnWitkL96g4', # Isaac
+    '0J19G5YWN6tlNJgP2sRkKd', # J.ADE
+    '6EybZ5zwPNEtEngfrEMevN', # JSOL
+    '3KJopZ2uUclqEtLxQg0FNn', # Jack - J97
+    '4ZSYaCMxF2k6maN3KTP17F', # JayKii
+    '3MZsBdqDrRTJihTHQrO6Dq', # Joji
+    '51wGpVg4Z3lv1IjuZAQjXn', # Juky San
+    '5bJcxnt5lkrDyKfPjtSBxP', # Jun Phạm
+    '3rjcQ5VIWCN4q7UFetzdeO', # JustaTee
+    '2ZN2K5xz3BDmfWYaA5dgce', # JustaTee
+    '3E6LGptA8lBEXPHAQCE3vr', # Kai Đinh
+    '2sv6rfo3OVxmRKSYJXWE3M', # Kairon
+    '2Bwp23pD4UVsSkchHDZw4F', # Karik
+    '3EkXXkwTWhJpPDJR4ME3D8', # KayC
+    '6tWaQSIL7reD85PKStNk28', # Kha
+    '26M9jrskgx7RHFydfAiyq5', # Khang Viet
+    '2xyEAgqFAAy5yqRZ7mHd9c', # Khiem
+    '71Cy7nzbfpuGJAS5FGxm93', # Khoi Vu
+    '3Ess3HrOUbKlRvQoGA1xT7', # Khả Hiệp
+    '2mx5AAdmlMxMcrcd7AQh1j', # Khắc Hưng
+    '7luDDYsfkSivBsoTz3BKMq', # Khắc Việt
+    '05xxXziuRhVsmlSjTD4Soh', # Kuun Đức Nam
+    '5kT9ftAJylK1roRMcdcsw2', # LAMOON
+    '70swmqj7c3SHkkdf2SrSUy', # LYHAN
+    '1Bn3dlrtgiD6zDzRSDBaMi', # Lala Trần
+    '2FCxeoDrmuTZmyn8iXlQVB', # Linh Hương Luz
+    '3Wqnr2JGruE4ruWNlnuiRE', # Lohan
+    '75Ki5hBCOpDtKGoFyTvLxP', # Lou Hoàng
+    '6TITnFVRcl0AcZ4syE7Toe', # Low G
+    '0QRUD41ltP8vhaAIxMZb2H', # Lu
+    '4POahBr8YCPNKGErIiofyx', # Lyly
+    '7H9jPV9qWyp6V629038aXU', # Lynk Lee
+    '39HlpQmjGAer2XWNh0Ro83', # Lãng
+    '3frEbqapobDOWI7JexlrTW', # Lê Bảo Bình
+    '5li8A5IiLMLhqGNpd7putm', # Lê Gia Bảo
+    '4sOvsEM0ooH149joyEm7Wg', # Lê Hiếu
+    '6hkqtv6Qx5AfoTGjBolcxQ', # Lương Quý Tuấn
+    '5X3f1KSripcxM3ECDNdHTt', # MAI PHUONG
+    '2rKtPYcwYmevNcaqzJOwvf', # MANBO
+    '2iYUs3FoNodbZXKf4mkjGM', # MAYDAYs
+    '0IdAjS2LRieBR3gzoazdAw', # MIN
+    '5jTFNkoZebPnX90CbB3F2A', # MONO
+    '2CLdtsyD2NsTOBBOVr7fi8', # MONSTAR
+    '1OIYKbmhG0RxPXvVPNj3NN', # Madihu
+    '4WBjLLe7H2Rqha7SSA0rLY', # Mai Tiến Dũng
+    '3nGqUwkJHiLPDECMVrX1Sq', # Masew
+    '0s9oqVCJ2iXTqSUR5fcky5', # Mer
+    '76nos2JmvLyYGZ28ribrf7', # MiiNa
+    '5zT1Ku9mzzl6RW5y4z0hUK', # Minh Huy
+    '4NfuHLESitkh66LOZeyzsu', # Minh Đinh
+    '4e5LAUvM35jleGg8gElTsP', # Miu Lê
+    '2nmxgv9asWhQh4XgqfxK57', # Mochiii
+    '35HU1GT1q797EwZsP8uduO', # Mr.Siro
+    '0Y9KzsdFByEI4bigKF1htd', # Myra Trần
+    '21trGUfOb8qkRjI4EyF1qm', # Mỹ Mỹ
+    '1CWwyDPjCowRTO4p6A7r6g', # Mỹ Tâm
+    '5QbDgpkghCaWjJywjbJnoG', # NB3 Hoài Bảo
+    '1MTANbmLhUqJXlLuqADGE7', # NIT
+    '4gMi1Penv4Tq4Wv5eb0TN8', # Nana Liu
+    '4fg8aMZ23d2bxKz7r2vt9v', # Negav
+    '1uXHYAZLyOQY5qaM1crBNK', # Nguyen Van Chung
+    '0FflNqUFk4ODtAZuVuqgou', # Nguyenn
+    '5Ib3D8UtLdYZjhVNWzwfoH', # Nguyên Hà
+    '2T8CUdrA64je3QHfcB0uYJ', # Nguyên Vũ
+    '7oxw8G3qB7DwjCs18JWJt5', # Nguyễn Hùng
+    '50zVd0WBgPZqC88gkrJ5xd', # Nguyễn Hữu Kha
+    '2a8CbNT5XhhygZtuAg3PdT', # Nguyễn Kiều Oanh
+    '1e2soLvkyYiXMugNDo0VNt', # Nguyễn Thành Đạt
+    '0AOAMHZgHaxM71om8IDGsv', # Nguyễn Trung Đức
+    '2UCPMrvFm42t3w5duYfQHl', # Nguyễn Trọng Tài
+    '4JsnmbP09WaZJoqL7HtePc', # Nguyễn Vĩ
+    '7lvyx4ByebHqtDV0BJSvcd', # Nguyễn Vĩ
+    '6tsfwoZBQVBTrGmSBOuCaK', # Nguyệt Chan
+    '2ukex9mJuIbR6msM02Flwf', # Ngân Ngân
+    '7at0gkjCnhOBrxwQlJPJ4s', # Ngô Kiến Huy
+    '6VLN4hTrGOZ5Le1TVTwDTg', # Ngọc Dolil
+    '0V2DfUrZvBuUReS1LFo5ZI', # Ngọt
+    '4usb4zmoHVcrvPPHHkKYaV', # Nhâm Phương Nam
+    '3mibIJiduF0MVLLAvHZAxw', # Noo Phước Thịnh
+    '0ZbgKh0FgPYeFP38nVaEGp', # Obito
+    '4APrfmUo8KRrjCVuyoKvwY', # Olew
+    '5GBXwBVQufRCmwI1bNRIUo', # Only C
+    '5lAfakPZgxFKgiJD6xAF1G', # Orange
+    '6sJBvMCAi9NuNCxI9RDYDW', # Osad
+    '5yE1xvNIbuEB2kkopCuCVY', # PAR SG
+    '3HD3V1nGFPwepBO7064Ij0', # PC
+    '2ldAFJhpkWvdSsgDR4EkJE', # Phan Duy Anh
+    '0aNyWetkjBvIdZvJY00yEa', # Phan Mạnh Quỳnh
+    '0qkGQFUrj8ERPAZcdpa79j', # Phuc Du
+    '4en2ObzSYlIYbCGknXjgG9', # Pháo
+    '13c4ijEkXNvEFoxGxwp5Su', # Pháp Kiều
+    '38QmAddjWUR2zKkdp3Oe87', # Phát Huy T4
+    '1OnnVNvbz0OsApDXHUX2bM', # Phí Phương Anh
+    '23Gbd21nBxC1kCtcG2Cd0C', # Phùng Khánh Linh
+    '5Kh0ta0UY4uJ4g2CIdq9V9', # Phương Ly
+    '1BcjfrXV4Oe3fK0c8dnxFF', # Phương Mỹ Chi
+    '6fvwPdTHctaY65nhFeodrs', # Phương Thanh
+    '3yCPgp6ckDEqBUQnybWeTm', # Phạm Kỳ
+    '0uV9wXh91Nuk5YHYqlkVup', # Phạm Lịch
+    '2Xlia1jlI7JDki4Xa42uyK', # Phạm Nguyên Ngọc
+    '033tY0EKEVdDtzoeEMMCJC', # Phạm Quỳnh Anh
+    '0hHw0uD1VFCrmJZGhrYqKA', # Phạm Vũ Luân
+    '5Xn0cJrrcslCWCzPXNTy54', # PiaLinh
+    '64pk9TSotiH3VRJc2oEHhz', # Pinky Vanh
+    '2dC1IBU9NOD9xsQJ5RE7j7', # Puppy
+    '5OSU5tRR0M59TppFcvl4s8', # Quang Hà
+    '0slOzRzTQb1RBBVJbvRITP', # Quang Hùng MasterD
+    '2LX2SKJhvZjyH4Z7XMptjd', # Quang Đăng Trần
+    '2NRcG7E1j2sSi8vnUzCcpi', # Quân A.P
+    '2xyztcPkhFKucM2ghmSme2', # Quốc Thiên
+    '1cPpd989kghzlZqfpP4KwL', # RAP VIỆT
+    '1W2pigH6UYwuTuFbwoSfVZ', # RHYDER
+    '4dH6uW4FU977FDuTcCHGUf', # RIO
+    '1zSv9qZANOWB4HRE8sxeTL', # RPT MCK
+    '25JRC8aMGIGBIJ5KiINv3l', # RPT Orijinn
+    '3eWWKcc07vz4dghL63VHFY', # Rayo
+    '0gGd4WhPXBSgDX6fdOHcOw', # Rhymastic
+    '0FvZX3MHDrlAAnf15FgBkA', # Richie D. ICY
+    '4jQZaxfgwiUJFQagCyZNV4', # Ricky Star
+    '0dBcEvEklr1jx4uZuhFX5e', # Ronboogz
+    '6CGGvCBHWqQ4HXtn5aLhbh', # SOOBIN
+    '2BoU3GzyaG397l5ci1twa9', # Shartnuss
+    '4WF9GdetrQiLeWDWAISRGa', # Shayda
+    '3hRnvEdu1iPyB6EzMBbuZq', # Shiki
+    '4SiNg3BrvdFycwTlO6HGKN', # Show It All Vietnam (Tân Binh Toàn Năng)
+    '3gdsVSks0f5ifaXSBbSZEO', # Sixkie Dawgz
+    '0obXkAXG0Ivhjj6TK6QZfd', # Sofia
+    '7qM9nMv6HFHFUpiN034R3z', # SonaOne
+    '0iqum9KAHh3LkgDhuqvTxM', # Song Luân
+    '0CkuQCslREF7yuvAqJXVuz', # Sáo
+    '5dfZ5uSmzR7VQK0udbAVpf', # Sơn Tùng M-TP
+    '2rVYwIYNEsMxtt8j5yemHb', # T.R.I
+    '063N27mzFn4rYsaFwUsRgc', # THIÊN ĐÌNH
+    '26qIfHV2sbOiRqapo4b2Cu', # TLong
+    '2knyDFP4xw9wZEWA98JX6b', # TRANG
+    '4npVRqpqmmM0MYHXTzu0o1', # Tama
+    '3oD2yI1g6aT3KawHD8ZYaU', # TeuYungBoy
+    '14W31zJumZnGDgZuPXclTJ', # Thanh Hưng
+    '0u5ikKYYDO2XGyYjNGQRGf', # The Flob
+    '1dwfi5uS5pfQMhAic73Q7L', # The Masked Singer
+    '53LVvmM09CnWWAYz79HpCR', # The Men
+    '5mPRDRL2rclNaPJXOg56Jy', # The Wind
+    '6RUfMGubOnHT1p3nfeXpBl', # Thiều Bảo Trâm
+    '2ipMqlG0XTQbUbu7W0FnBm', # Thành Đạt
+    '6BnKArbWE6Il0AyLJdvUxy', # Thái Học
+    '7lq25uVNvzRoAJgr4CYr8e', # Thái Đinh
+    '1oD9fKbb7qQ2nhn9JJC24F', # Thắng
+    '2xK6qcvfXuFFbU0NL95aeo', # Thịnh Suy
+    '5OvCh1Nin8AGw7OkxYinBe', # Tiên Tiên
+    '055tKHSCudU5z9QjDbxUox', # Tony Hoàng Anh
+    '0USsiioabw7ukSY0ZBwXlP', # Trung Hiếu
+    '5Cf9nBDNc99UFkq9Yqap7Q', # Trung Quân
+    '6Ue0BbbTOdBjcO4QZB5sio', # Trungg I.U
+    '1dmaHHd1Xbd85cT1egYb4w', # Trúc Nhân
+    '4co1OIKlUOsNNVJFSZzO9N', # Trịnh Thăng Bình
+    '73jYB754ez8rGnHM79YeuE', # Trịnh Đình Quang
+    '04xz9bIBYTIqEpmRvQkfvu', # Tyronee
+    '23xCr75lKvnFvKi8ImHE7S', # Táo
+    '4YkqEuVf1Jf2x2XDqJ2CvC', # Tóc Tiên
+    '4rBvN6UImNzcY5abMKdAuw', # Tùng Dương
+    '1n9JKdEdLxrbgJiqz1WZFJ', # Tăng Duy Tân
+    '2KUMXzsDcJqXZW0CRh07zP', # Uyên Linh
+    '22MX8BG5870DGzpNricY7q', # VSOUL
+    '2aMo0CYbTieTisS0BlWc59', # VSTRA
+    '0OECMkZp1koXqQLQrMizHb', # Vicky Nhung
+    '0cf0oidLIQoNhGTp0SHzhU', # Việt Anh
+    '63SNH9m8M034lCGELVC1dm', # Văn Mai Hương
+    '7yquVKfxBuNFJbG9cy2R8A', # Vũ Cát Tường
+    '7MsHVZKh1TeKgGqm65kZnY', # Vũ Duy Khánh
+    '70ULEgr5A3zBL12foM4bnj', # Vũ Phụng Tiên
+    '57g2v7gJZepcwsuwssIfZs', # Vũ.
+    '351mBX7BjjI2plbfozIzqb', # VƯƠNG BÌNH
+    '3OeYidiG0MAcGmiSwTHZf6', # Vương Anh Tú
+    '6NF9Oa4ThQWCj6mogFSrVD', # W/N
+    '30eFAXoU2kTjJPf2cq80B8', # WEAN
+    '0qAJ604EZMbkbpZ9NN6W6d', # WOKEUP
+    '5zqSvInhVbM7ogkSfxny2C', # Wendy Thao
+    '3HMWKU43x9GxkGWVP1KjQT', # Will
+    '5M3ffmRiOX9Q8Y4jNeR5wu', # Wren Evans
+    '1Jmkhzve9TuXHUUWGmpANw', # Wxrdie
+    '2zdwhQ4jqSm8BJf6rpR2Bb', # Yến Nhi
+    '7Cp2hGcriAaDUAWpXnSEwm', # buitruonglinh
+    '6kkA6laUMR0hzML9YY98CQ', # kidsai
+    '3y6Lfjkrdn5TgkbgDxbpvM', # marzuz
+    '1bweJ8ReP08akfsoW8jiP9', # nae & de\'lay
+    '3diftVOq7aEIebXKkC34oR', # tlinh
+    '0agWx6vy0Ss4FGafO8bEYc', # Ánh Sáng AZA
+    '3Myoe574gxIKLU5bKqjkFk', # Ân ngờ
+    '5A3lLFvd6YIIpn3oy3yFM9', # Út Nhị Mino
+    '5XcfMxmHUiyxc1Q283Nmxj', # Đan Trường
+    '1LEtM3AleYg1xabW6CRkpi', # Đen
+    '2VLOCK5ffcQAuyo30Es0xj', # Đinh Tiến Đạt
+    '1vzuR7EBzvfrqlSkOZlL6c', # Đoàn Lâm
+    '4ht0wODL01ELRxlDYvsFad', # Đàm Vĩnh Hưng
+    '6JTiPLdbZD2e0tDsN15U1s', # Đông Nhi
+    '4R3mugkUqCALXgkwSptTbg', # Đạt G
+    '37yCp3F1wG0gNrnaLkIdDV', # Đậu Tất Đạt
+    '3eKnh5bh0TzO9WwnKMJplK', # Đặng Thiên Chí
+    '5HoKHEC3OTaMgMDnCqc00B', # Đặng Vĩnh Thịnh
+    '0TZKKZG6WHl2aVFPDtND82', # Độ Mixi
+    '5FWPIKz9czXWaiNtw45KQs', # Đức Phúc
+    '4R2engiAed6QeIV63PDAz0', # Đức Trường
+]
